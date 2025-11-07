18d5b96c8df93d89cdf72b01ebbeb31057093ac2
Khoa Tran <ovn6002@gmail.com>
2025-11-07 17:29:08 +0700

    chore(demo/vuln-app): add rebuild script and switch vuln-app port to 5001
    - add `rebuild.sh` to stop and rebuild services
    - set `PODMAN_COMPOSE_WARNING_LOGS=false` in script to reduce noise
    - update `demo/compose.yml` and `demo/vulnerable-app/app.py` to use port 5001 (host & container)

---
+++ demo/rebuild.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+export PODMAN_COMPOSE_WARNING_LOGS=false
+set -e
+cd "$(dirname "$0")"
+
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+TARGET=${1:-all}
+
+# validate input
+if [[ ! $TARGET =~ ^(vuln|hard|all)$ ]]; then
+  echo "invalid target $TARGET"
+  echo "usage: $0 [ vuln | hard | all ]"
+  exit 1
+fi
+
+# for podman & docker compatibility
+if command -v podman &> /dev/null; then
+    TOOL=podman
+elif command -v docker &> /dev/null; then
+    TOOL=docker
+else
+    echo "Error: podman or docker is not installed." >&2
+    exit 1
+fi
+
+echo
+echo "--- Stopping containers ---"
+$TOOL compose down -v
+
+echo
+if [[ $TARGET =~ ^(all)$ ]]; then
+  echo "--- Rebuilding all apps ---"
+  $TOOL compose build --no-cache
+else
+  echo "--- Rebuilding $TARGET-app ---"
+  $TOOL compose build --no-cache "$TARGET-app"
+fi
+
+echo
+echo " ✔ REBUILD COMPLETED"
+
+echo
+echo "--- Starting containers ---"
+$TOOL compose up -d
+
+sleep 0.25
+
+echo
+echo "---"
+sleep 0.25
+$TOOL compose ps
